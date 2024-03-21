6efea969e2bac290aad719ca762ceb2030e3bd76
Alexia C <aleixadebora@gmail.com>
2024-03-21 14:13:51 -0300

    new

---
+++ learning.go
@@ -0,0 +1,10 @@
+func mains() {
+	type Coordenada struct {
+		latitude float64
+		longitude float64
+	}
+}
+
+coordenada := Coordenada{latitude: 37.42, longitude: -122.08}
+
+fmt.Println(coordenada)
