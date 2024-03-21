e894835477c5275ce6c2a105089fd2f0e0bca850
Alexia C <aleixadebora@gmail.com>
2024-03-21 13:55:27 -0300

    adding new files

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for contador := 10; contador != 0; contador-- {
+		fmt.Println("Contagem Regressiva:", contador)
+	}
+
+	fmt.Println("Arrasou!")
+}
